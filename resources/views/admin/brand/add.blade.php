fc972682e6dc2333096646624678abf1699b0039
ahar <uddin2007112@stud.kuet.ac.bd>
2024-05-24 13:09:52 +0600

    added

--- resources/views/admin/brand/add.blade.php
+++ resources/views/admin/brand/add.blade.php
@@ -9,7 +9,7 @@
     <div class="container-fluid">
       <div class="row mb-2">
         <div class="col-sm-12">
-          <h1>Add New Brand</h1>
+          <h1>Add Brand</h1>
         </div>
        
       </div>
